352c97fe566614e517885e54ac0bebe05c6e9b17
cheetax <dmitriy.grebenev@gmail.com>
2018-08-25 23:38:08 +0300

    new

--- src/App.js
+++ src/App.js
@@ -10,26 +10,26 @@ import './App.css';
 
 class App extends Component {
 
-  
+
   render() {
     const date = new Date()
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Test Input Field</h1>
         </header>
-        <NumberField onSpinButtons onChange={(event) => console.log(event)} name='label' value={2018} type='number' label='Label' />
-        <DateField outlined onSpinButtons onChange={(event) => console.log(event)} name='label' value={new Date()} label='Data' />
+        <div className='App-intro' >
+          <NumberField onSpinButtons onChange={(event) => console.log(event)} name='label' value={2018} type='number' label='Label' />
+          <DateField outlined onSpinButtons onChange={(event) => console.log(event.target.value)} name='label' value={new Date()} label='Data' />
+        </div>
+
         {/* <Calendar date={date} toClose={false} onSelect={(date) => {
           console.log(date)
         }} />
         <Calendar isMonth date={date} toClose={false} onSelect={(date) => {
           console.log(date)
         }}/> */}
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
