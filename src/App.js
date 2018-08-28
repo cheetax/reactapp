3c2a660661916e1a81bd1b45c10059375bdc0350
cheetax <dmitriy.grebenev@gmail.com>
2018-08-28 13:43:08 +0300

    new

--- src/App.js
+++ src/App.js
@@ -13,6 +13,15 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Test Input Field</h1>
         </header>
+        <h3>Filled text fields</h3>
+        <div className='App-intro' >
+          <NumberField onSpinButtons onChange={(event) => console.log(event)} name='label' value={123456789} type='number' label='Label' />
+          <DateField onCalendarButton onSpinButtons onChange={(event) => console.log(event.target.value)} name='date' value={date} label='Date' />
+          <MonthField onCalendarButton onSpinButtons onChange={(event) => console.log(event.target.value)} name='month' value={date} label='Month' />
+          <QuarterField onSpinButtons onChangeObject={(obj) => console.log(obj)} name='quarter' value={date} label='Quarter' /> 
+          <YearField onSpinButtons onChangeObject={(obj) => console.log(obj)} name='year' value={date} label='Year' />
+        </div>
+        <h3>Outlined text fields</h3>
         <div className='App-intro' >
           <NumberField outlined onSpinButtons onChange={(event) => console.log(event)} name='label' value={123456789} type='number' label='Label' />
           <DateField outlined onCalendarButton onSpinButtons onChange={(event) => console.log(event.target.value)} name='date' value={date} label='Date' />
